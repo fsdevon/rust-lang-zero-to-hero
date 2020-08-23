5340b949a77b695f7e6ac97f8502f881ca64166c
fsdevon <fullstackdevovernight@gmail.com>
2020-08-23 16:00:44 +0700

    Improved with iterators.

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,16 +1,15 @@
 use std::env;
 use std::process;
 
-use minigrep::Config;
+use minigrep_fsdevon::Config;
 
 fn main() {
-  let args: Vec<String> = env::args().collect();
-  let config = Config::new(&args).unwrap_or_else(|err| {
+  let config = Config::new(env::args()).unwrap_or_else(|err| {
     eprintln!("Problem parsing arguments: {}", err);
     process::exit(1);
   });
 
-  if let Err(e) = minigrep::run(config) {
+  if let Err(e) = minigrep_fsdevon::run(config) {
     eprintln!("Application error: {}", e);
     process::exit(1);
   }
